57b6ff460e790df840c3595f102070c7adfcb597
kazi-a <kazi.arafat@hotmail.com>
2024-10-17 18:57:30 -0400

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("ContactUs")]
+        public IActionResult ContactUs()
+        {
+            var contact = new Dictionary<string, string>
+        {
+            { "Phone", "555-123-4567" },
+            { "Email", "info@myguitarshop.com" },
+            { "Address", "123 Guitar Lane, Music City, USA" },
+            { "Social Media", "@myguitarshop" }
+        };
+
+            return View(contact);
+        }
+    }
+}
